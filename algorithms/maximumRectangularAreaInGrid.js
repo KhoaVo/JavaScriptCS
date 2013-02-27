d4166b0c150b18c0acf4ec814ae8eae1a15f5cee
KhoaVo <vokhoa408@gmail.com>
2013-02-27 12:01:17 -0800

    Update algorithms/maximumRectangularAreaInGrid.js

--- algorithms/maximumRectangularAreaInGrid.js
+++ algorithms/maximumRectangularAreaInGrid.js
@@ -29,122 +29,154 @@
     [1,1,1,1,1]
  ]);
 
- console.log(solver.solve()); // should output 5
-
+ console.log(solver.run()); // should output 5
  solver.addToGrid([1,1,0,0,0]);
+ console.log(solver.run()); // should output 6
 
- console.log(solver.solve()); // should output 6
 
+You can run the above example by doing
+
+ MaxRectangularAreaInGrid.test();
 
  */
 
 /*
     @param valueToLookFor   denotes the value to look for when seeing if a section is contiguous
  */
-var MaxRectangularAreaInGrid = function(valueToLookFor){
-    this.grid = [];
-    this.value = valueToLookFor;
-};
 
-MaxRectangularAreaInGrid.prototype = {
-    constructor: MaxRectangularAreaInGrid,
+(function(root){
 
-    solve: function(){
-        var self = this,
-            grid = this.grid,
-            curColHeights = [],
-            MAX = 0;
-
-        grid.forEach(function(row){
-            self._adjustHeights(curColHeights,row);
-            MAX = Math.max(MAX,self._findLargestFromColumnHeights(curColHeights));
-        });
-
-        return MAX;
-    },
-
-    /* Sets the grid to be solved */
-    setGrid: function(grid){
-        this.grid = grid;
-    },
-
-    /* Add a row to the grid to be solved */
-    addToGrid: function(row){
-        this.grid.push(row);
-    },
+    var MaxRectangularAreaInGrid = function(valueToLookFor){
+        this.grid = [];
+        this.value = valueToLookFor;
+    };
 
 
-    /*
-     Adjusts the max column heights based on the addition of a new row
-     */
-    _adjustHeights: function (curColHeights,newRow){
-        var toLookFor = this.value;
-        newRow.forEach(function(val,j){
-            if(val === toLookFor)
-                curColHeights[j] ? curColHeights[j]++ : curColHeights[j] = 1;
-            else
-                curColHeights[j] = 0;
-        });
-    },
+    MaxRectangularAreaInGrid.prototype = {
+        constructor: MaxRectangularAreaInGrid,
 
-    /*
-        Based on the column heights we can solve for the largest rectangular area by
-        in linear O(n) time.
+        run: function(){
+            var self = this,
+                grid = this.grid,
+                curColHeights = [],
+                MAX = 0;
 
-        @param colHeights   a one dimensional array representing the column heights so far. EX: [1,0,3,1,5,5,2,1,4,7]
+            grid.forEach(function(row){
+                self._adjustHeights(curColHeights,row);
+                MAX = Math.max(MAX,self._findLargestFromColumnHeights(curColHeights));
+            });
 
-     */
-    _findLargestFromColumnHeights: function (colHeights){
+            return MAX;
+        },
 
-        var MAX = 0,longest = 0, highest = 0,
-            toDo = [],solved = {},
-            l = colHeights.length,start = 0,
-            h,cur;
+        /* Sets the grid to be solved */
+        setGrid: function(grid){
+            this.grid = grid;
+        },
 
-        //find the first column with a non zero height
-        while(colHeights[start++] === 0);
-        if(colHeights[--start])
-            toDo.push([colHeights[start],start]);
+        /* Add a row to the grid to be solved */
+        addToGrid: function(row){
+            this.grid.push(row);
+        },
 
-        while(toDo.length){
-            cur = toDo.pop();
-            longest = 0;
-            highest = cur[0];
-            start = cur[1];
 
-            //mark that we solved this start index already
-            solved[start] = true;
+        /*
+         Adjusts the max column heights based on the addition of a new row
+         */
+        _adjustHeights: function (curColHeights,newRow){
+            var toLookFor = this.value;
+            newRow.forEach(function(val,j){
+                if(val === toLookFor)
+                    curColHeights[j] ? curColHeights[j]++ : curColHeights[j] = 1;
+                else
+                    curColHeights[j] = 0;
+            });
+        },
 
-            for(start = cur[1]; start < l && (h = colHeights[start]);start++){
-                longest++;
+        /*
+         Based on the column heights we can solve for the largest rectangular area by
+         in linear O(n) time.
 
-                //if the next column is currently higher than our max we
-                //put it in stack so that we know to come back and start from that
-                //index later
-                if(h > highest && !solved[start])
-                    toDo.push([h,start]);
+         @param colHeights   a one dimensional array representing the column heights so far. EX: [1,0,3,1,5,5,2,1,4,7]
 
-                //if the next column is lower than the current column
-                //we downgrade our best height for current block.
-                else if(h < highest)
-                    highest = h;
+         */
+        _findLargestFromColumnHeights: function (colHeights){
 
-                MAX = Math.max(MAX,highest * longest);
-            }
+            var MAX = 0,longest = 0, highest = 0,
+                toDo = [],solved = {},
+                l = colHeights.length,start = 0,
+                h,cur;
 
+            //find the first column with a non zero height
             while(colHeights[start++] === 0);
             if(colHeights[--start])
                 toDo.push([colHeights[start],start]);
+
+            while(toDo.length){
+                cur = toDo.pop();
+                longest = 0;
+                highest = cur[0];
+                start = cur[1];
+
+                //mark that we solved this start index already
+                solved[start] = true;
+
+                for(start = cur[1]; start < l && (h = colHeights[start]);start++){
+                    longest++;
+
+                    //if the next column is currently higher than our max we
+                    //put it in stack so that we know to come back and start from that
+                    //index later
+                    if(h > highest && !solved[start])
+                        toDo.push([h,start]);
+
+                    //if the next column is lower than the current column
+                    //we downgrade our best height for current block.
+                    else if(h < highest)
+                        highest = h;
+
+                    MAX = Math.max(MAX,highest * longest);
+                }
+
+                while(colHeights[start++] === 0);
+                if(colHeights[--start])
+                    toDo.push([colHeights[start],start]);
+            }
+
+            return MAX;
         }
+    };
 
-        return MAX;
-    }
-}
+    /*
+        Test cases
+     */
+    MaxRectangularAreaInGrid.test = function(){
+        var solver = new MaxRectangularAreaInGrid(1);
+        solver.setGrid([
+            [1,1,0,0,1],
+            [1,1,1,1,1]
+        ]);
+        console.log(solver.run()); // should output 5
+        solver.addToGrid([1,1,0,0,0]);
+        console.log(solver.run()); // should output 6
+    };
 
 
-if(typeof module !== "undefined" && module.exports)
-    module.exports = MaxRectangularAreaInGrid;
+    if(typeof module !== "undefined" && module.exports)
+            exports = module.exports = MaxRectangularAreaInGrid;
+    else
+        root.MaxRectangularAreaInGrid = MaxRectangularAreaInGrid;
 
-if ( typeof define === "function" && define.amd) {
-    define( "maxrectangularareaingrid", [], function () { return MaxRectangularAreaInGrid; } );
-}
+    if ( typeof define === "function" && define.amd)
+        define( "maxrectangularareaingrid", [], function () { return MaxRectangularAreaInGrid; } );
+
+})(this);
+
+
+
+
+
+
+
+
+
